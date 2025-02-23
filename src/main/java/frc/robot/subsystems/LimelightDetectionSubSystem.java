30662f17f2f1f864d56844aaed3aeb2ac88480ff
Nathaniel <nexnotone@proton.me>
2025-02-23 13:44:08 -0800

    lockout for LimelightDetectionSubSystem

--- src/main/java/frc/robot/subsystems/LimelightDetectionSubSystem.java
+++ src/main/java/frc/robot/subsystems/LimelightDetectionSubSystem.java
@@ -4,6 +4,7 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 //import frc.robot.Constants.LimelightConstants;
+import edu.wpi.first.wpilibj2.command.Commands;
 
 public class LimelightDetectionSubSystem {
 
@@ -12,22 +13,32 @@ public class LimelightDetectionSubSystem {
     double ty = table.getEntry("ty").getDouble(0.0);
     double ta = table.getEntry("ta").getDouble(0.0);
 
+    boolean bButton = false;
+    boolean aimLockout = false;
 
     public LimelightDetectionSubSystem() {
 
     }
 
-
-    boolean bButton = false;
+    public void initLockout(boolean bButton) {
+        if (bButton == false) {
+            aimLockout = false;
+        } else if (bButton == true) {
+            aimLockout = true;
+            Commands.waitSeconds(7);
+            aimLockout = false;
+        }
+    }
 
     public void aimAssist(boolean bButton) {
-        if (bButton) {
-            double heading_error = -tx;
-            double distance_error = -ty;
-            double steering_adjust = 0.0f;
+        if (aimLockout == false) {
+            if (bButton == false && aimLockout == false) {
+                double heading_error = -tx;
+                double distance_error = -ty;
+                double steering_adjust = 0.0f;
 
         // tank drive -- rewrite for swerve
-            /*
+        /*
             if (tx > 1.0) {
                 steering_adjust = LimelightConstants.KpAim * heading_error - LimelightConstants.MinAimCommand;
             } else if (tx < -1.0) {
@@ -38,8 +49,9 @@ public class LimelightDetectionSubSystem {
 
             left_command += steering_adjust + distance_adjust;
             right_command -= steering_adjust + distance_adjust;
-            */
-            
+        */
+
+            }
         }
     }
 
